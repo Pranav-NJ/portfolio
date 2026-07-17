4b259e8e76e823d48c712f0dbca478cd65368af4
Pranav NJ <pranav984530@example.com>
2026-07-17 10:30:34 +0530

    Update resume and projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -32,8 +32,8 @@ const PROJECTS = [
     desc: 'A modern and responsive portfolio built with React and Framer Motion, showcasing projects, skills, and achievements with smooth animations and interactive UI.',
     ss: '/portfolio.jpg',
     tech: ['React', 'Framer Motion', 'Tailwind CSS'],
-    live: '#',
-    code: 'https://github.com/Pranav-NJ/'
+    live: 'https://pranavnj-portfolio.vercel.app/',
+    code: 'https://github.com/Pranav-NJ/portfolio'
   },
   {
     title:'Server-Monitor',
